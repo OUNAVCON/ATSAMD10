e51196a7881f6fb8165161941df6291f56a23446
Isaac Rose <isaacdrose@gmail.com>
2018-04-09 20:49:53 -0400

    commented out example of using GCLK_IO[] output on IO pin. Mux H

--- src/gpio.c
+++ src/gpio.c
@@ -3,9 +3,9 @@
 
 void initGpio(){
     PORT_DIRSET(PORTA) = 0x40 | 0x80; //PA06 (0x40) PA08(0x80)
- //   PORT_DIRSET(PORTA) |= 0x80; //PA08
-    PORT_MUX(PORTA,8) = 0x07;//Function H => GCLK_IO[0]
-    PORT_PINCFG(PORTA,8) |= 0x1;
+//   PORT_DIRSET(PORTA) |= 0x80; //PA08
+//    PORT_MUX(PORTA,8) = 0x07;//Function H => GCLK_IO[0]
+//    PORT_PINCFG(PORTA,8) |= 0x1;
 }
 
 void toggleLed(){
